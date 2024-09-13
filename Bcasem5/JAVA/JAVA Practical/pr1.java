13b13079f38426c031f4de06461c9dbb1dcd4fe1
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-09-13 12:11:20 +0530

    Update pr1.java

--- Bcasem5/JAVA/JAVA Practical/pr1.java
+++ Bcasem5/JAVA/JAVA Practical/pr1.java
@@ -5,22 +5,27 @@ import java.util.Scanner;
 
 public class pr1 {
     public static void main(String[] args) {
-        // int a ;
-        // double pi  =Math.PI;
+         int a ;
+         double pi  =Math.PI;
 
-        // System.out.println("enter the number of area of circle:");
-        // Scanner sc = new Scanner(System.in);
-        // a = sc.nextInt();
+         System.out.println("enter the number of area of circle:");
+         Scanner sc = new Scanner(System.in);
+         a = sc.nextInt();
 
-        // double area = pi * a *a;
+         double area = pi * a *a;
 
-        // System.out.println("area of circle : "+area);
+         System.out.println("area of circle : "+area);
         
     
-        int [][]arr = {{1,2},{3,4}};
+      //  int [][]arr = {{1,2},{3,4}};
 
-        System.out.println(arr[0][0]);
-        System.out.println(arr[1][1]);
+       // System.out.println(arr[0][0]);
+      //  System.out.println(arr[1][1]);
 
     }
 }
+
+OUTPUT:
+enter the number of area of circle: 5
+
+area of circle :78.53981633974483
