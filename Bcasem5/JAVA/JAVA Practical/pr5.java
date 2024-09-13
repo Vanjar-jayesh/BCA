0d27412bbf3eb07c3532d9117bd8ee0165723746
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-09-13 12:18:19 +0530

    Update pr5.java

--- Bcasem5/JAVA/JAVA Practical/pr5.java
+++ Bcasem5/JAVA/JAVA Practical/pr5.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package practicaljava;
 
 import java.util.Scanner;
@@ -46,53 +46,8 @@ public class pr5 {
         }
     }
 }
-=======
-package practicaljava;
+OUTPUT:
+enter day:
+    4
+    thu..
 
-import java.util.Scanner;
-
-public class pr5 {
-    public static void main(String[] args) {
-
-        int day;
-
-        System.out.println("enter day:");
-        Scanner sc = new Scanner(System.in);
-        day = sc.nextInt();
-
-        // write a java program that shows the digits statement
-        switch (day) {
-            case 1:
-                System.out.println("mon..");
-                break;
-                
-            case 2:
-            System.out.println("tue..");
-            break;
-            
-            case 3:
-                System.out.println("wed..");
-                break;
-                
-            case 4:
-            System.out.println("thu..");
-            break;
-            
-            case 5:
-                System.out.println("fri..");
-                break;
-                
-            case 6:
-            System.out.println("sat..");
-            break;
-        
-            case 7:
-                System.out.println("sun..");
-                break;
-            default:
-            System.out.println("Invlied day");
-                break;
-        }
-    }
-}
->>>>>>> d57630d95a0eb09d765f2fa6ae036adaf297c42c
